4e0f645088251baed5de469303ea963a801409dc
britneywang <118696500+britneywang@users.noreply.github.com>
2023-12-07 17:36:20 -0500

    Partners12/7

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/HotDeals.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/HotDeals.java
@@ -2,7 +2,10 @@ package edu.psu.ist.hcdd340.finalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,13 +14,39 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HotDeals extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private HotDeals_adapter mAdapter;
+
+    private final CampusData[] CAMPUS_LIST = {
+            new CampusData("Abba Java", R.drawable.abbajava, " • Closes 5PM", "(814)532-1423", "Open" ),
+            new CampusData("Lion Pantry", R.drawable.lionpantry, " • Opens 4PM", "(814)765-3457", "Closed"),
+            new CampusData("Waring Commons", R.drawable.waringcommons, " • Closes 8PM", "(814)187-7425", "Open"),
+            new CampusData("Redifer Commons", R.drawable.redifer, " • Closes 8PM", "(814)867-6234", "Open"),
+            new CampusData("Pollock Commons", R.drawable.pollock, " • Closes 8PM", "(814)432-6132", "Open"),
+            new CampusData("Findlay Commons", R.drawable.findlay, " • Closes 8PM", "(814)153-6213", "Open"),
+            new CampusData("Warnock Commons", R.drawable.warnock, " • Closes 8PM", "(814)413-523", "Open"),
+            new CampusData("Warnock Commons", R.drawable.warnock, " • Closes 8PM", "(814)413-523", "Open")
+
+
+    };
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hot_deals);
+        setContentView(R.layout.hot_deals_locations);
+
+        mRecyclerView = findViewById(R.id.recycler_view_campus);
+        mAdapter = new HotDeals_adapter(this, CAMPUS_LIST);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 
-        // Initialize and assign variable
+
+    // Initialize and assign variable
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottomNavigationView);
 
         // Set Hot Deals selected
---
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/HotDeals_adapter.java
@@ -0,0 +1,115 @@
+package edu.psu.ist.hcdd340.finalproject;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HotDeals_adapter extends
+        RecyclerView.Adapter<HotDeals_adapter.CampusViewHolder> {
+    private final CampusData[] mCampusList;
+    private final LayoutInflater mInflater;
+    public HotDeals_adapter(Context context, CampusData[] mCampusList) {
+        this.mCampusList = mCampusList;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    public class CampusListActivity extends AppCompatActivity {
+        private RecyclerView mRecyclerView;
+        private HotDeals_adapter mAdapter;
+
+
+    }
+
+
+    public HotDeals_adapter(CampusData[] mCampusList, LayoutInflater mInflater) {
+        this.mCampusList = mCampusList;
+        this.mInflater = mInflater;
+    }
+
+
+
+
+    class CampusViewHolder extends RecyclerView.ViewHolder {
+        public final TextView campusItemView;
+        final HotDeals_adapter mAdapter;
+        public final TextView campusEmailView;
+        public final TextView campusPhoneView;
+        public final ImageView campusImageView;
+
+        public final TextView status;
+
+        public
+
+        CampusViewHolder(View campusItemView1, HotDeals_adapter mAdapter) {
+            super(campusItemView1);
+
+
+            campusItemView = campusItemView1.findViewById(R.id.campus_list_title_id);
+            // this.campusItemView = campusItemView;
+            this.mAdapter = mAdapter;
+            campusImageView = itemView.findViewById(R.id.campus_list_image_id);
+            campusEmailView = itemView.findViewById(R.id.campus_list_email);
+            campusPhoneView = itemView.findViewById(R.id.campus_list_phone);
+            status = itemView.findViewById(R.id.status);
+        }
+    }
+
+    @NonNull
+    @Override
+    public HotDeals_adapter.CampusViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.hot_deals_list_item,
+                parent, false);
+        return new CampusViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HotDeals_adapter.CampusViewHolder holder, int position) {
+
+
+        CampusData campus = mCampusList[position];
+        holder.campusItemView.setText(campus.getCampusName());
+
+        // display phone ->
+        holder.campusPhoneView.setText(campus.getPhone());
+
+        // display email ->
+        holder.campusEmailView.setText(campus.getEmail());
+
+        // display profile ->
+        int image = campus.getProfileImage();
+        holder.campusImageView.setImageResource(image);
+
+        // display hours  ->
+        String status = campus.getHours();
+        holder.status.setText(status);
+
+        // set text color based on status
+        campus.setRedTextColor(holder.status);
+        campus.setGreenTextColor(holder.status);
+
+
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCampusList.length;
+    }
+}
+
+
+
+
+
+
+
