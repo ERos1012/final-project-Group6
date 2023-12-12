53a05aab3aa21328085ed0fdc7667ef53325ac0c
jjb6692 <69400015+jjb6692@users.noreply.github.com>
2023-12-11 22:47:34 -0500

    Update LoginActivity.java
    Added code for data storage if user registers their information

--- app/src/main/java/edu/psu/ist/hcdd340/finalproject/LoginActivity.java
+++ app/src/main/java/edu/psu/ist/hcdd340/finalproject/LoginActivity.java
@@ -15,35 +15,67 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private static final String TAG = "ACTIVITY_LOGIN";
+    public static final String EMAIL_KEY = "EMAIL";
+    public static final String PASSWORD_KEY = "PASSWORD";
+    public static final String SHARED_PREF_NAME = "USERNAME";
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
         Button buttonLogin = findViewById(R.id.button_login);
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText editTextEmail = findViewById(R.id.editTextEmail);
-                String email = editTextEmail.getText().toString();
+                int id = v.getId();
+                if (id == R.id.button_register) {
+                    registerUser();
+                    Button button = findViewById(R.id.button_register);
+                    Snackbar.make(button,
+                            "Registered!",
+                            Snackbar.LENGTH_LONG).show();
+                }
+                else if (id == R.id.button_login) {
+                    EditText editTextEmail = findViewById(R.id.editTextEmail);
+                    String email = editTextEmail.getText().toString();
 
-                EditText editTextPassword = findViewById(R.id.editTextPassword);
-                String password = editTextPassword.getText().toString();
+                    EditText editTextPassword = findViewById(R.id.editTextPassword);
+                    String password = editTextPassword.getText().toString();
 
-                // Validate the username and password
-                if (isValidEmail(email) && isValidPassword(password)) {
-                    // Login successful
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    // Invalid username or password
-                    Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    // Validate the username and password
+                    if (isValidEmail(email) && isValidPassword(password)) {
+                        // Login successful
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // Invalid username or password
+                        Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
     }
 
+    String getInputFromEditText(int id) {
+        EditText v = findViewById(id);
+        return v.getText().toString();
+    }
+    void registerUser() {
+        String email = getInputFromEditText(R.id.editTextEmail);
+        String password = getInputFromEditText(R.id.editTextPassword);
+        Log.d(TAG, "Email: " + email + ", Password: " + password);
+        saveUserInformation(email, password);
+    }
+    void saveUserInformation(String email, String password) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(EMAIL_KEY, email);
+        editor.putString(PASSWORD_KEY, password);
+        editor.apply();
+    }
+    
     private boolean isValidEmail(String email) {
         return email.contains("@") && email.length() >= 6;
     }
